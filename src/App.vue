3c97663669c6a1e4cc858c05a0b57f21fb6aa19f
Wang Lei <lei.wang1@thoughtworks.com>
2019-10-15 22:28:28 +0800

    add todo input

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>todos</h1>
+    <new-todo></new-todo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewTodo from "./components/NewTodo.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    NewTodo
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 500px;
 }
 </style>
---
+++ src/components/NewTodo.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-input class="input" v-model="input" placeholder="What needs to be done?">
+    <el-button slot="prepend" icon="el-icon-arrow-down"></el-button>
+  </el-input>
+</template>
+<script>
+import Vue from "vue";
+import { Input } from "element-ui";
+
+Vue.use(Input);
+
+export default {
+  data() {
+    return {
+      input: ""
+    };
+  }
+};
+</script>
+<style scoped>
+.input {
+  font-size: 18px;
+}
+</style>
