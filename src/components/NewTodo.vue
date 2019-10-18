5bd6c6c6d9af6ac74793465fed1171b1fe5cd01f
Wang Lei <lei.wang1@thoughtworks.com>
2019-10-18 20:52:49 +0800

    add feature to add todo when press enter key

--- src/components/NewTodo.vue
+++ src/components/NewTodo.vue
@@ -1,8 +1,9 @@
 <template>
-  <el-input class="input" v-model="input" placeholder="What needs to be done?">
-    <el-button slot="prepend" icon="el-icon-arrow-down" @click="handleCheckAll"></el-button>
-    <el-button slot="append" icon="el-icon-plus" @click="handleAddTodo"></el-button>
-  </el-input>
+  <form @submit="handleAddTodo($event)">
+    <el-input class="input" v-model="input" placeholder="What needs to be done?">
+      <el-button slot="prepend" icon="el-icon-arrow-down" @click="handleCheckAll"></el-button>
+    </el-input>
+  </form>
 </template>
 <script>
 export default {
@@ -13,7 +14,8 @@ export default {
   },
   methods: {
     handleCheckAll() {},
-    handleAddTodo() {
+    handleAddTodo(event) {
+      event.preventDefault();
       this.$emit("add-todo", this.input);
       this.input = "";
     }
